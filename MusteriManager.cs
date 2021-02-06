8a513ff585e64db923d42b0977875e0303abed6b
Güldeniz Özdemir <78044804+Deniz000@users.noreply.github.com>
2021-02-06 14:45:12 +0300

    First commit

---
+++ MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        //operasyon tutuyor demek - managerlar
+        public static void Ekle(Musteri musteri)
+        {
+            musteri.Numarasi = 4385;
+            Console.WriteLine("Eklenecek Kişinin Adı :");
+            musteri.Ad = Console.ReadLine();
+
+            Console.WriteLine("Eklenecek Kişinin Adı :");
+            musteri.Soyad = Console.ReadLine();
+
+            Console.WriteLine(musteri.Ad +" " + musteri.Soyad + " Eklendi!");
+        }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Numarasi = 1245;
+            musteri1.Ad = "Cemile";
+            musteri1.Soyad = "Yazmalı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Numarasi = 4568;
+            musteri2.Ad = "Hakkı";
+            musteri2.Soyad = "Polat";
+
+            Musteri musteri3 = new Musteri();
+            MusteriManager.Ekle(musteri3);
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2, musteri3}; // veritabanından alır
+
+            //typesafe--tip güvenli : tipi olmak zorunda
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Numarasi + " " + musteri.Ad +" "+ musteri.Soyad );
+            }
+           
+
+        }
+    }
+}
